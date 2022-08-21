5ee98fb86dbecdfe9ffce746a146348bf0165283
Uchmahn <uchechukwuazubuine@gmail.com>
2022-08-21 14:33:00 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3c967160-57a5-4a4e-88eb-4cf276ac3726';
+export const MODAL_CLOSE_SEC = 2.5;
