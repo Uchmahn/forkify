4ad7daed6fac0765b4f4893d63fc049e609b6b0a
Uchmahn <uchechukwuazubuine@gmail.com>
2022-08-28 18:56:07 +0100

    TEST23 reset by Key Change

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '3c967160-57a5-4a4e-88eb-4cf276ac3726';
+export const KEY = 'e5861525-68d8-41c2-ae5f-1130ba78293e';
 export const MODAL_CLOSE_SEC = 2.5;
